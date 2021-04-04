b8c7494e91b912e325f8345907b4de8aa22ff2b3
Brian Crisp <bcrisp084@yahoo.com>
2021-04-03 21:25:55 -0400

    set up port and intial choice list

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password123$",
+    database: "employees_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    getStarted()
+})
+
+const getStarted = () => 
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'action',
+        message: 'Please decide how you would like to proceed.',
+        loop: false,
+        choices: [
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Delete a Department",
+        "Delete a Role",
+        "Delete an Employee",
+        "View employees by manager",
+        "Update employee's role",
+        "Update employee's manager",
+        "View the total utilized budget of a department",
+        ]
+    },
+]);
